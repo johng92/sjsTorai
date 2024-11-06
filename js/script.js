cd19707ba6d465809810651279e93b08e108771e
John Hembrom <johng3337@outlook.com>
2024-11-06 18:54:06 +0530

    dynamic teams & static gallery carousel added

--- js/script.js
+++ js/script.js
@@ -8,3 +8,76 @@ const headerEl = document.querySelector('.header');
 btnNavEl.addEventListener('click', function(){
     headerEl.classList.toggle('nav-open');
 })
+/////////////////////////////////
+// CAROUSEL WITH SWIPER JS FOR TEAMS//////
+/////////////////////////////////
+new Swiper('.cards-wrapper ', {
+    loop: true,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    //Pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive Breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+  });
+
+
+/////////////////////////////////
+// CAROUSEL WITH SWIPER JS FOR GALLERY//////
+/////////////////////////////////
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+
+    //Pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive Breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+  });
+
