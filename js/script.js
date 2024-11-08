b527d180c5ce29a7ef1eecb64cc5305c580df932
John Hembrom <johng3337@outlook.com>
2024-11-08 12:39:56 +0530

    Some footer elements made dynamic

--- js/script.js
+++ js/script.js
@@ -117,3 +117,7 @@ new Swiper('.card-wrapper', {
     }
   });
 
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
